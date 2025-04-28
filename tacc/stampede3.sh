4f4d5b256351bdb47e0d6f4f5a7b2cca1d122054
amnp95 <amnp95@uw.edu>
2025-04-28 12:11:21 -0700

    refactor stampede3.sh to improve formatting and organization of dependency installation

--- tacc/stampede3.sh
+++ tacc/stampede3.sh
@@ -2,11 +2,18 @@ clear
 ml phdf5
 rm -rf build
 
+# build an envirormrnt and activate it (not necessary, but recommended)
+
+# installing the dependencies
+pip install numpy==1.23.0 scipy==1.11.1 matplotlib geopy pyproj tqdm
+
+
 export CC=icx
 export CXX=icpx
 export FC=ifx
 export F77=ifx
 export F90=ifx
+
 
 python3 stampede3.py build
 # python3 stampede3.py install
